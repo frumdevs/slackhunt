54a46ad4979382c8d1ddf416d265a15c62cd42a1
Avraham David Gelbish <adg@adgelb.fish>
2018-07-13 00:58:56 -0400

    switched environment variable name

--- server-src/app.ts
+++ server-src/app.ts
@@ -2,7 +2,7 @@ import * as express from 'express'
 
 const app = express();
 
-const port = process.env.SLACKHUNT_API_SERVER_PORT || 3001
+const port = process.env.PORT || 3001
 
 // tslint:disable-next-line:no-console
 app.listen(port, () => console.log(`api server listening on ${port}`))
